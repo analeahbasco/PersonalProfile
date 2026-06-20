2af4d89251ce883d9635aca782cd9b99bcb109ea
nngn12 <analeahbasco1@gmail.com>
2026-06-20 18:07:04 +0800

    Additional design

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
     hamburger.classList.toggle("active");
 });
 
-links.forEach(link => {
+document.querySelectorAll(".nav-links li").forEach(link => {
     link.addEventListener("click", () => {
         navLinks.classList.remove("active");
         hamburger.classList.remove("active");
