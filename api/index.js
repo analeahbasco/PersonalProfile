8e67150adfec79742e00f7b0e0e8775ae8b548c6
nngn12 <analeahbasco1@gmail.com>
2026-06-30 13:46:48 +0800

    export express app for vercel serverless

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
-import app from "../server/server.js";
+// --- SERVER START / VERCEL EXPORT ---
+if (process.env.NODE_ENV !== 'production') {
+    // This runs on your local computer
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
 
-export default app;
+// This is strictly for Vercel
+module.exports = app;
